491b30ebc7cec878ca4b15925e4a309ff2a37465
Recep Varlık <recep.varlik@gmail.com>
2025-09-01 15:44:38 +0300

    functions(iyzico-payment): CORS’a x-debug başlığını ekle; OPTIONS preflight’ı geçsin

--- supabase/functions/iyzico-payment/index.ts
+++ supabase/functions/iyzico-payment/index.ts
@@ -3,7 +3,7 @@ import Iyzipay from "npm:iyzipay";
 Deno.serve(async (req) => {
     const corsHeaders = {
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+        'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-debug',
         'Access-Control-Allow-Methods': 'POST, OPTIONS',
         'Access-Control-Max-Age': '86400'
     };
