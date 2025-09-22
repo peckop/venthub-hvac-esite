536148d2c49f3400bb80ebe679474f866395c4df
Recep Varlık <recep.varlik@gmail.com>
2025-09-22 09:41:07 +0300

    revert(perf): kritik CSS bloğunu kaldır, hero görsel kalitesini yükselt, build ayarlarını stabilize et
    - index.html içindeki ağır inline critical CSS kaldırıldı (muhtemel CLS/TBT etkisini önlemek için)
    - HeroSection görsel kalite değerleri artırıldı (AVIF 60, WebP 75, JPG 85)
    - Vite build: assetsInlineLimit 4096'a geri alındı; deneysel target/cssMinify satırları kaldırıldı
    
    Amaç: LCP kalitesini yükseltirken skor regresyonunu geri almak

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,16 +5,16 @@ const InViewCounter = React.lazy(() => import('./InViewCounter'))
 // Responsive hero variants generated at build-time via vite-imagetools
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import avifSet from '../../public/images/industrial_HVAC_air_handling_unit_warehouse.jpg?w=640;960;1200&format=avif&quality=40&srcset'
+import avifSet from '../../public/images/industrial_HVAC_air_handling_unit_warehouse.jpg?w=640;960;1200&format=avif&quality=60&srcset'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import webpSet from '../../public/images/industrial_HVAC_air_handling_unit_warehouse.jpg?w=640;960;1200&format=webp&quality=55&srcset'
+import webpSet from '../../public/images/industrial_HVAC_air_handling_unit_warehouse.jpg?w=640;960;1200&format=webp&quality=75&srcset'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import jpgSet from '../../public/images/industrial_HVAC_air_handling_unit_warehouse.jpg?w=640;960;1200&format=jpg&quality=65&srcset'
+import jpgSet from '../../public/images/industrial_HVAC_air_handling_unit_warehouse.jpg?w=640;960;1200&format=jpg&quality=85&srcset'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import hero1200 from '../../public/images/industrial_HVAC_air_handling_unit_warehouse.jpg?w=1200&format=jpg&quality=65'
+import hero1200 from '../../public/images/industrial_HVAC_air_handling_unit_warehouse.jpg?w=1200&format=jpg&quality=85'
 
 const HeroPicture: React.FC = () => {
   const sizes = '(max-width: 640px) 100vw, (max-width: 1024px) 90vw, 1200px'
