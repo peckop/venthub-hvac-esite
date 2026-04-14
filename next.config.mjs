2f92d78a575360f2bebd5d123285ee09689d8150
Recep Varlık <recep.varlik@gmail.com>
2026-04-14 14:23:44 +0300

    chore(config): disable bundle analyzer browser popup

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import bundleAnalyzer from '@next/bundle-analyzer';
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: false,
 });
 
 /** @type {import('next').NextConfig} */
